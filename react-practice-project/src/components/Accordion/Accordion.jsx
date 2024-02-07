bdab08a35b9193180944cba876c09de502744890
116495254+MIbrahim71@users.noreply.github.com <Ibrahim_M789@outlook.com>
2024-02-07 20:08:23 +0000

    Load More finished

---
+++ react-practice-project/src/components/Accordion/Accordion.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+
+export default function Accordion() {
+  const [selected, setSelected] = useState(false);
+  const [multi, setMulti] = useState(false);
+
+  function handleClick(accordionNo) {
+    setSelected(selected === accordionNo ? null : accordionNo);
+  }
+
+  function handleSelectAll() {
+    setMulti((multi) => !multi);
+  }
+
+  return (
+    <div>
+      <button onClick={() => handleSelectAll()}>Select All</button>
+      <div className="accordion" onClick={() => handleClick(1)}>
+        <div className="title">
+          <p>What are accordion components?</p>
+          <button className="plus">+</button>
+        </div>
+        {(selected === 1 || multi) && (
+          <div className="content">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+          </div>
+        )}
+      </div>
+      <div className="accordion" onClick={() => handleClick(2)}>
+        <div className="title">
+          <p>What are they used for?</p>
+
+          <button className="plus">+</button>
+        </div>
+        {(selected === 2 || multi) && (
+          <p className="content">
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+            nisi ut aliquip ex ea commodo consequat.
+          </p>
+        )}
+      </div>
+      <div className="accordion" onClick={() => handleClick(3)}>
+        <div className="title">
+          <p>Accordion is a musical instrument</p>
+          <button className="plus">+</button>
+        </div>
+        {(selected === 3 || multi) && (
+          <p className="content">
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+            nisi ut aliquip ex ea commodo consequat.
+          </p>
+        )}
+      </div>
+    </div>
+  );
+}
---
+++ react-practice-project/src/components/LoadMore/LoadMore.jsx
@@ -0,0 +1,72 @@
+import { useEffect, useState } from "react";
+import "./index.css";
+
+export default function LoadMore({ url, limit = 20, skip = 0 }) {
+  const [loading, setLoading] = useState(false);
+  const [products, setProducts] = useState([]);
+  const [count, setCount] = useState(0);
+  const [disable, setDisable] = useState(false);
+
+  async function fetchProducts(url) {
+    try {
+      setLoading(true);
+      const response = await fetch(
+        `${url}?limit=${limit}&skip=${count === 0 ? 0 : count * 20}`
+      );
+      const data = await response.json();
+      console.log(data);
+
+      if (data?.products?.length) {
+        setProducts((prevData) => {
+          const newProducts = data.products.filter((newProduct) => {
+            // Check if the product ID already exists in the existing products to ensure products are not appended multiple times
+            // ensures only unique products are added, mapped only once
+            return !prevData.some(
+              (existingProduct) => existingProduct.id === newProduct.id
+            );
+          });
+          return [...prevData, ...newProducts];
+        }); // load new and keep old products
+        setLoading(false);
+      }
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchProducts(url);
+  }, [url, count]);
+
+  useEffect(() => {
+    if (products?.length === 100) setDisable(true);
+  }, [products]);
+
+  if (loading) {
+    return <div>Loading data...</div>;
+  }
+
+  return (
+    <div className="container">
+      {products &&
+        products.map((product) => (
+          <div className="product-container" key={product.id}>
+            <img src={product.thumbnail} alt="" />
+            <h1>{product.title}</h1>
+            <p>{product.description}</p>
+            <p>£{product.price}</p>
+          </div>
+        ))}
+      <div className="s">
+        <button
+          disabled={disable}
+          className="load"
+          onClick={() => setCount(count + 1)}
+        >
+          {disable ? "No more products" : "Load More"}
+        </button>
+      </div>
+    </div>
+  );
+}
