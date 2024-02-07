8be2a678f2c985f00a92176e35e039e4c4a433db
116495254+MIbrahim71@users.noreply.github.com <Ibrahim_M789@outlook.com>
2024-02-07 16:11:27 +0000

    ImageSlide complete

--- react-practice-project/src/components/ImageSlider/ImageSlider.jsx
+++ react-practice-project/src/components/ImageSlider/ImageSlider.jsx
@@ -8,6 +8,15 @@ export default function ImageSlider({ url, limit = 1, page = 4 }) {
   const [errorMessage, setErrorMessage] = useState(null);
   const [loading, setLoading] = useState(false);
 
+  function handlePrevious() {
+    setCurrentSlide(currentSlide === 0 ? images.length - 1 : currentSlide - 1);
+  }
+
+  function handleNext() {
+    setCurrentSlide(currentSlide === images.length - 1 ? 0 : currentSlide + 1);
+  }
+
+  // Fetch images from API
   async function fetchImages(getUrl) {
     try {
       setLoading(true);
@@ -24,12 +33,16 @@ export default function ImageSlider({ url, limit = 1, page = 4 }) {
     }
   }
 
+  // useEffect
+  // ensures fetchImages function is called whenever 'url' changes
+  // fetchImages is an asynchronous function that produces side effects. Placing it inside a useEffect hook separates side effects from component logic
+  // By specifying [url] as a dependency it prevents potential infinite loops that would've occurred if 'fetchImages' was called unconditionally in the component body
+  // Component code becomes cleaner and easier to read - separates concerns & makes code more maintainable
   useEffect(() => {
     if (url) fetchImages(url);
   }, [url]);
 
-  console.log(images);
-
+  // Conditional rendering of 'loading' and 'errorMessage' ensures UI reflects loading and error states properly
   if (loading) {
     return <div>Loading data...</div>;
   }
@@ -40,22 +53,43 @@ export default function ImageSlider({ url, limit = 1, page = 4 }) {
 
   return (
     <div className="container">
-      <BsArrowLeftCircleFill className="arrow arrow-left" />
+      <BsArrowLeftCircleFill
+        onClick={handlePrevious}
+        className="arrow arrow-left"
+      />
+      {/* Images mapped - if current slide is equal to the mapping index, show image via class. If not, hide the image */}
       {images &&
-        images.map((imageItem) => (
+        images.map((imageItem, index) => (
           <img
             key={imageItem.id}
             src={imageItem.download_url}
             alt={imageItem.download_url}
-            className="current-image"
+            className={
+              currentSlide === index
+                ? "current-image"
+                : "current-image hide-current-image"
+            }
           />
         ))}
-      <BsArrowRightCircleFill className="arrow arrow-right" />
+      <BsArrowRightCircleFill
+        onClick={handleNext}
+        className="arrow arrow-right"
+      />
+
       <span className="circles">
+        {/* Circle indicator mapping - onClick set the currentSlide to the index of the circle */}
         {images &&
-          images.map((_, index) => {
-            <button key={index} className="current-indicator"></button>;
-          })}
+          images.map((_, index) => (
+            <button
+              key={index}
+              className={
+                currentSlide === index
+                  ? "current-indicator"
+                  : "current-indicator inactive"
+              }
+              onClick={() => setCurrentSlide(index)}
+            ></button>
+          ))}
       </span>
     </div>
   );
